d2c814277650acd8d1457bafc88676ed87e27b6f
Josh Levy <joshua.levy@tmz.com>
2017-12-21 12:39:27 -0800

    Remove rename-poll and poll-renamed Apollo schemas and build files

--- build/php/src/Acme/Schemas/Apollo/Event/PollRenamed.php
+++
@@ -1,9 +0,0 @@
-<?php
-// @link http://acme-schemas.triniti.io/json-schema/acme/apollo/event/poll-renamed/latest.json#
-namespace Acme\Schemas\Apollo\Event;
-
-use Gdbots\Pbj\Message;
-
-interface PollRenamed extends Message
-{
-}
