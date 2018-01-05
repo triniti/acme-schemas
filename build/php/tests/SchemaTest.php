c5819cff395591dfa7a7b0be2c2425b08e12493d
Josh Levy <joshua.levy@tmz.com>
2018-01-05 11:19:30 -0800

    Add unit tests for valid and invalid asset mime types

--- build/php/tests/SchemaTest.php
+++ build/php/tests/SchemaTest.php
@@ -6,6 +6,7 @@ use Gdbots\Pbj\Message;
 use Gdbots\Pbj\MessageRef;
 use Gdbots\Pbj\MessageResolver;
 use PHPUnit\Framework\TestCase;
+use Acme\Schemas\Dam\Node\ImageAssetV1;
 
 class SchemaTest extends TestCase
 {
@@ -33,5 +34,71 @@ class SchemaTest extends TestCase
 
             $this->assertTrue(ArrayUtils::isAssoc($message->getUriTemplateVars()));
         }
+    }
+
+    /**
+     * @expectedException \Gdbots\Pbj\Exception\AssertionFailed
+     */
+    public function testAssetMimeTypeNoForwardSlash()
+    {
+        ImageAssetV1::create()->set('mime_type', 'imagejpg');
+    }
+
+    /**
+     * @expectedException \Gdbots\Pbj\Exception\AssertionFailed
+     */
+    public function testAssetMimeTypeInvalidCharBeforeSlash()
+    {
+        ImageAssetV1::create()->set('mime_type', '%image/jpg');
+    }
+
+    /**
+     * @expectedException \Gdbots\Pbj\Exception\AssertionFailed
+     */
+    public function testAssetMimeTypeInvalidCharAfterSlash()
+    {
+        ImageAssetV1::create()->set('mime_type', 'image/j$pg');
+    }
+
+    /**
+     * @expectedException \Gdbots\Pbj\Exception\AssertionFailed
+     */
+    public function testAssetMimeTypeDoesNotAllowSpaces()
+    {
+        ImageAssetV1::create()->set('mime_type', 'image/ jpg');
+    }
+
+    public function testAssetMimeTypeAllowsDigits()
+    {
+        ImageAssetV1::create()->set('mime_type', '1image/1jpg');
+        $this->assertTrue(true, 'Accepted valid mime type contains digits');
+    }
+
+    public function testAssetMimeTypeAllowsDash()
+    {
+        ImageAssetV1::create()->set('mime_type', 'application/octet-stream');
+        $this->assertTrue(true, 'Accepted valid mime type');
+    }
+
+    public function testAssetMimeTypeAllowsDots()
+    {
+        ImageAssetV1::create()->set('mime_type', 'application/vnd.hzn-3d-crossword');
+        $this->assertTrue(true, 'Accepted valid mime type');
+    }
+
+    public function testAssetMimeTypeAllowsPlus()
+    {
+        ImageAssetV1::create()->set('mime_type', 'application/vnd.adobe.air-application-installer-package+zip');
+        $this->assertTrue(true, 'Accepted valid mime type');
+    }
+
+    /**
+     * @expectedException \Gdbots\Pbj\Exception\AssertionFailed
+     * @todo: Should this fail, or should we allow a universal subtype?
+     */
+    public function testAssetMimeTypeAllowsStar()
+    {
+        ImageAssetV1::create()->set('mime_type', 'application/*');
+        $this->assertTrue(true, 'Accepted valid mime type');
     }
 }
