5a2a1faf5c8678dadc1785ff8cb5da274c1f36d4
Josh Levy <joshua.levy@tmz.com>
2018-01-09 15:45:45 -0800

    Remove MarkAssetAs* and AssetMarkedAs* build files

--- build/php/src/Acme/Schemas/Dam/Command/MarkAssetAsDraft.php
+++
@@ -1,9 +0,0 @@
-<?php
-// @link http://acme-schemas.triniti.io/json-schema/acme/dam/command/mark-asset-as-draft/latest.json#
-namespace Acme\Schemas\Dam\Command;
-
-use Gdbots\Pbj\Message;
-
-interface MarkAssetAsDraft extends Message
-{
-}
