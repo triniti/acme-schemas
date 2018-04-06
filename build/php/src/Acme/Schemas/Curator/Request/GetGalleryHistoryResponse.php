406f061320a8a9946f21609aa0dca2967b91ea50
Eric Jacobson <contact@efjacobson.com>
2018-04-05 18:27:02 -0700

    response/request for gallery/promotion/teaser/timeline/widget and history for each as well

---
+++ build/php/src/Acme/Schemas/Curator/Request/GetGalleryHistoryResponse.php
@@ -0,0 +1,9 @@
+<?php
+// @link http://acme-schemas.triniti.io/json-schema/acme/curator/request/get-gallery-history-response/latest.json#
+namespace Acme\Schemas\Curator\Request;
+
+use Gdbots\Pbj\Message;
+
+interface GetGalleryHistoryResponse extends Message
+{
+}
---
+++ build/php/src/Acme/Schemas/Curator/Request/GetGalleryRequest.php
@@ -0,0 +1,9 @@
+<?php
+// @link http://acme-schemas.triniti.io/json-schema/acme/curator/request/get-gallery-request/latest.json#
+namespace Acme\Schemas\Curator\Request;
+
+use Gdbots\Pbj\Message;
+
+interface GetGalleryRequest extends Message
+{
+}
