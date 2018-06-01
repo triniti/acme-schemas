e7330c8405f1b1b93c9bebb8909074a9ec00af9c
weiwang314 <wei.wang@wbdevops.com>
2018-05-31 21:43:39 -0700

    command, event, request

---
+++ build/php/src/Acme/Schemas/Notify/Request/SearchNotificationsResponse.php
@@ -0,0 +1,9 @@
+<?php
+// @link http://acme-schemas.triniti.io/json-schema/acme/notify/request/search-notifications-response/latest.json#
+namespace Acme\Schemas\Notify\Request;
+
+use Gdbots\Pbj\Message;
+
+interface SearchNotificationsResponse extends Message
+{
+}
