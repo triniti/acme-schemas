aabfce37644568eabd1af0c8f0e17ca9a2f2be0c
weiwang314 <wei.wang@wbdevops.com>
2018-05-31 21:30:45 -0700

    add all nodes

---
+++ build/php/src/Acme/Schemas/Notify/Node/AlexaNotification.php
@@ -0,0 +1,9 @@
+<?php
+// @link http://acme-schemas.triniti.io/json-schema/acme/notify/node/alexa-notification/latest.json#
+namespace Acme\Schemas\Notify\Node;
+
+use Gdbots\Pbj\Message;
+
+interface AlexaNotification extends Message
+{
+}
---
+++ build/php/src/Acme/Schemas/Notify/Node/BrowserNotification.php
@@ -0,0 +1,9 @@
+<?php
+// @link http://acme-schemas.triniti.io/json-schema/acme/notify/node/browser-notification/latest.json#
+namespace Acme\Schemas\Notify\Node;
+
+use Gdbots\Pbj\Message;
+
+interface BrowserNotification extends Message
+{
+}
