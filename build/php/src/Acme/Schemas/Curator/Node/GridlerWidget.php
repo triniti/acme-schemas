a975e7a8e3d7d2aba4bc00233973352b87b4c9a5
Greg Brown <greg@12dnetworks.com>
2018-09-05 13:29:19 -0700

    Use  v1.5.6 and  v0.6.5. (#25)
    * Use  v1.5.6 and  v0.6.5.
    
    * add gridler-widget and tetris-widget

---
+++ build/php/src/Acme/Schemas/Curator/Node/GridlerWidget.php
@@ -0,0 +1,9 @@
+<?php
+// @link http://acme-schemas.triniti.io/json-schema/acme/curator/node/gridler-widget/latest.json#
+namespace Acme\Schemas\Curator\Node;
+
+use Gdbots\Pbj\Message;
+
+interface GridlerWidget extends Message
+{
+}
