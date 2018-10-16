6c6c888491e0d7ce06d00e5be453ce4f4623c6ea
weiwang314 <wei.wang@wbdevops.com>
2018-10-15 17:36:11 -0700

    remove vevo, twitter, pinerest

--- build/php/src/Acme/Schemas/Canvas/Block/TwitterMomentBlock.php
+++
@@ -1,9 +0,0 @@
-<?php
-// @link http://acme-schemas.triniti.io/json-schema/acme/canvas/block/twitter-moment-block/latest.json#
-namespace Acme\Schemas\Canvas\Block;
-
-use Gdbots\Pbj\Message;
-
-interface TwitterMomentBlock extends Message
-{
-}
