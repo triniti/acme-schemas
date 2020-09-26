aedfec33e922cd064f0cc9d56717a58b6b96b4ad
Eric Jacobson <contact@efjacobson.com>
2020-09-25 17:56:15 -0700

    v2.x - looking good, party time

--- build/php/tests/SchemaTest.php
+++ build/php/tests/SchemaTest.php
@@ -1,28 +1,88 @@
 <?php
 declare(strict_types=1);
 
+use Acme\Schemas\Dam\Node\ImageAssetV1;
+use Gdbots\Pbj\Exception\RequiredFieldNotSet;
 use Gdbots\Pbj\Message;
 use Gdbots\Pbj\MessageResolver;
 use Gdbots\Pbj\Util\ArrayUtil;
 use Gdbots\Pbj\WellKnown\MessageRef;
-use Gdbots\Pbj\WellKnown\NodeRef;
 use PHPUnit\Framework\TestCase;
 
 class SchemaTest extends TestCase
 {
-    public function testCanCreateAllMessages(): void
+    /**
+     * @throws \Throwable
+     */
+    public function testCanCreateAllMessages()
     {
-        /** @var Message|string $className */
+        /** @var Message $className */
         foreach (MessageResolver::all() as $curie => $className) {
             $message = $className::create();
             $this->assertInstanceOf($className, $message);
             $this->assertInstanceOf(Message::class, $message);
 
-            $ref = $message->generateMessageRef('tag');
-            $this->assertInstanceOf(MessageRef::class, $ref);
-            $this->assertSame($ref->toString(), $message->generateMessageRef('tag')->toString());
-            $this->assertInstanceOf(NodeRef::class, $message->generateNodeRef());
+            try {
+                $ref = $message->generateMessageRef('tag');
+                $this->assertInstanceOf(MessageRef::class, $ref);
+                $this->assertSame($ref->toString(), $message->generateMessageRef('tag')->toString());
+            } catch (RequiredFieldNotSet $e) {
+                // this is ok as some messages generate etags in their message
+                // refs which serialize the message and cause this exception
+            } catch (\Throwable $e) {
+                throw $e;
+            }
+
             $this->assertTrue(ArrayUtil::isAssoc($message->getUriTemplateVars()));
         }
+    }
+
+    public function getInvalidMimeTypeSamples()
+    {
+        return [
+            // No forward slash
+            ['imagejpg'],
+            // Invalid char in "type"
+            ['%image/jpg'],
+            // Invalid char in "subtype"
+            ['image/j$pg'],
+            // Space in mime type
+            ['image/ jpg'],
+            // Global mime type should be specific
+            ['image/*'],
+        ];
+    }
+
+    /**
+     * @dataProvider getInvalidMimeTypeSamples
+     *
+     * @expectedException \Gdbots\Pbj\Exception\AssertionFailed
+     */
+    public function testInvalidMimeTypesThrowException($sampleMimeType)
+    {
+        ImageAssetV1::create()->set('mime_type', $sampleMimeType);
+    }
+
+    public function getValidMimeTypeSamples()
+    {
+        return [
+            // Allow digits in type and subtype
+            ['1image/1jpg'],
+            // Allow dash in subtype
+            ['application/octet-stream'],
+            // Allow dots in subtype
+            ['application/vnd.hzn-3d-crossword'],
+            // Allow "+" char in subtype
+            ['application/vnd.adobe.air-application-installer-package+zip'],
+        ];
+    }
+
+    /**
+     * @dataProvider getValidMimeTypeSamples
+     */
+    public function testValidMimeTypes($sampleMimeType)
+    {
+        ImageAssetV1::create()->set('mime_type', $sampleMimeType);
+        $this->assertTrue(true, 'Accepted valid mime type');
     }
 }
